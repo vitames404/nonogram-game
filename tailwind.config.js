ff5c162d52b7169e23853f6c649dae1889bbe650
vitames404 <vitorames@protonmail.com>
2024-12-25 17:00:12 +1300

    Tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all subfolders and relevant file types
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
