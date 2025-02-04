57cc724412d2ec3a29629015f04c3c8793b9a084
vitames404 <vitorames@protonmail.com>
2025-02-04 23:07:26 +1300

    Loading page changed

---
+++ src/components/Loading.tsx
@@ -0,0 +1,25 @@
+import {motion} from "framer-motion"
+
+const Loading = () => {
+  return (
+    <div className="w-screen flex justify-center">
+      <motion.div className="text-white font-vt323 text-8xl relative"
+              animate={{
+                y: [-10, 10, -10], // Moves the div up and down
+              }}
+              transition={{
+                duration: 2, // Animation duration
+                repeat: Infinity, // Repeat infinitely
+                ease: "easeInOut", // Smooth transition
+              }}>
+        <div className="absolute top-[100px] md:top-[200px] transform -translate-x-1/2 -translate-y-1/2">
+          <p>
+          LOADING...
+          </p>
+        </div>
+      </motion.div>
+    </div>
+  );
+};
+
+export default Loading;
