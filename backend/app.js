ee4c7940a375238fd8b0470cf3a7c18e57f5fa82
vitames404 <vitorames@protonmail.com>
2025-02-04 22:32:56 +1300

    Backend routing bug test

--- backend/app.js
+++ backend/app.js
@@ -569,6 +569,12 @@ app.get('*', (req, res) => {
   }
 });
 
+app.use(express.static(path.join(__dirname, '..', 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
